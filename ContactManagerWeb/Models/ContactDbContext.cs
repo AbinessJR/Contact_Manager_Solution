dc700793c77025dada431b509872975ea69f14e0
Abiness JR <88325145+AbinessJR@users.noreply.github.com>
2023-09-14 19:26:56 +0530

    Create ContactDbContext.cs

---
+++ ContactManagerWeb/Models/ContactDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManagerWeb.Models
+{
+    public class ContactDbContext : DbContext
+    {
+        public ContactDbContext(DbContextOptions<ContactDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().ToTable("Contact");
+        }
+    }
+}
